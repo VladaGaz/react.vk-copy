6330c38298c1382b397a6139d42d86602060f0d7
VladaGaz <vladamuzyka7@gmail.com>
2024-03-06 09:23:56 +0300

    Add firebase.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import RoutesComponent from "./components/routes/Routes";
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjcLoJDUlPwLndu8L-i0dyqD1JbBdiC14",
+  authDomain: "vk-copy-65c7a.firebaseapp.com",
+  projectId: "vk-copy-65c7a",
+  storageBucket: "vk-copy-65c7a.appspot.com",
+  messagingSenderId: "144970771832",
+  appId: "1:144970771832:web:ca1c58e170acb1e5d22c8d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
